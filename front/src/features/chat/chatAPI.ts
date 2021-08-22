1ef01d75f681a1526f5a4ab1f29fbcd685db6267
sh1okoh <kooooo.0719task@gmail.com>
2021-08-22 14:01:37 +0900

    redux-toolkit の streaming apiを追加してみた

--- front/src/features/chat/chatAPI.ts
+++ front/src/features/chat/chatAPI.ts
@@ -1,1 +1,40 @@
-export {}
+import { build } from '@reduxjs/toolkit/dist/query/core/buildMiddleware/cacheLifecycle';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import socketClient  from "socket.io-client";
+
+import { Channel, Message } from './interface';
+
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  endpoints: (build) => ({
+    getMessages: build.query<Message[], Channel>({
+      query: (channel) => `messages/${channel}`,
+      async onCacheEntryAdded(
+        arg,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) {
+        const socket = socketClient('http://localhost:3000');
+        try {
+          await cacheDataLoaded
+
+          const listener = (event: MessageEvent) => {
+            const data = JSON.parse(event.data)
+            if (data.channel !== arg) return
+            updateCachedData((draft) => {
+              draft.push(data)
+            })
+          }
+        } catch {
+
+        }
+
+        await cacheEntryRemoved
+
+        socket.close();
+      }
+    })
+  })
+});
+
+export const { useGetMessagesQuery } = api;
--- front/src/features/chat/interface.ts
+++ front/src/features/chat/interface.ts
@@ -4,3 +4,13 @@ export interface ChatState {
   },
   status: 'loading' | 'succeeded' | 'failed' | 'idle',
 };
+
+export interface Message {
+  id: number
+  channel: Channel
+  userName: string
+  text: string
+}
+
+export type Channel = 'redux' | 'general';
+
