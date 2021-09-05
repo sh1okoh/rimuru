38c829a35e3ed3acd355fc066e1c4f7a0f0d98f3
sh1okoh <kooooo.0719task@gmail.com>
2021-09-05 22:12:10 +0900

    refactor: リファクタ

--- api/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { login, logout } from './auth';
-
-
-// login
-const loginRouter = Router();
-loginRouter.post('/login', login);
-loginRouter.get('/logout', logout)
-
